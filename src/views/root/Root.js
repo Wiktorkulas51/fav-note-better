7bd73b1b53164e4675ce11d0c7bbf6d89fe1a258
Wiktorkulas51 <wiktorkulas51@gmail.com>
2020-11-25 17:32:34 +0100

    styled components

---
+++ src/views/root/Root.js
@@ -0,0 +1,16 @@
+import React from "react";
+import GlobalStyle from "../../theme/GlobalStyle";
+import Button from "../../components/button/Button";
+
+function Root() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>lul</h1>
+      <Button>close / save</Button>
+      <Button secondary>remove</Button>
+    </div>
+  );
+}
+
+export default Root;
